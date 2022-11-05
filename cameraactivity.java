35668908282ea9052338adbeb13e4c4a0b5affb1
vasupati <67497606+vasupati@users.noreply.github.com>
2022-11-04 23:34:16 -0400

    Create cameraactivity.java

---
+++ cameraactivity.java
@@ -0,0 +1,27 @@
+public  class CameraActivity{
+         // Instance Variables
+        int imageRequestCode;
+         int videoRequestCode;
+         int ScanBarcode;
+         
+ //methods
+public void captureImage(){
+system.out.print(“capture image”);
+}
+public void recordVideo(){
+system.out.print(“record video”);
+}
+public void previewVideo(){
+system.out.print(“preview video”);
+}
+public void previewCaptureImage(){
+system.out.print(“preview capture image”);
+}
+public void captureBarcode(){
+system.out.print(“scan barcode”);
+}
+public void previewBarcode(){
+system.out.print(“preview barcode”);
+}
+}
+        
