705bf5e34a3d41e1d08d011aa271b2ce100b23f9
vasupati <67497606+vasupati@users.noreply.github.com>
2022-11-04 23:36:16 -0400

    Create actuator.java

---
+++ actuator.java
@@ -0,0 +1,11 @@
+public void moveforward(){
+system.out.print(“move forword”);
+}
+public void moveleft(){
+system.out.print(“move left”);
+}
+public void setspeed(){
+system.out.print(“sets speed”);
+}
+
+
